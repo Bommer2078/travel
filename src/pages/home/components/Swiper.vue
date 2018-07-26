a817d50494265c4030f9df020744830ddcb9789e
bommer <897694309@qq.com>
2018-07-26 23:20:27 +0800

    commit swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,25 @@
+<template>
+  <swiper :options="swiperOption">
+     <!--slides -->
+    <swiper-slide>
+      img
+    </swiper-slide>
+     <!--Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+    export default {
+        name: "homeSwiper",
+        data(){
+          return {
+            swiperOption:{}
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
