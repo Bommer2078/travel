96dc751e32f48b16681bb8fa5de26093aa3787b2
bommer <897694309@qq.com>
2018-07-27 17:08:57 +0800

    change swiper

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="wrapper">
     <swiper :options="swiperOption">
-      <swiper-slide>
-        <img src="@/assets/pics/8e4a1c3f470d3702.jpg_640x170_c1e8baeb.jpg">
-      </swiper-slide>
-      <swiper-slide>
-        <img src="@/assets/pics/44fce1467be17702.jpg_640x170_ae6ad5e0.jpg" height="170" width="640"/>
-      </swiper-slide>
-      <swiper-slide>
-        <img src="@/assets/pics/858481b3e2d65802.png_640x170_90ebc386.png" height="170" width="640"/>
-      </swiper-slide>
-      <swiper-slide>
-        <img src="@/assets/pics/c72a1ccd4d7b2202.jpg_640x170_a4d1cf68.jpg" height="170" width="640"/>
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img :src="item.src">
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
@@ -26,7 +17,13 @@
             swiperOption:{
               pagination: '.swiper-pagination',
               loop:true
-            }
+            },
+            swiperList:[
+              {id:'001',src:'/static/pics/banner/8e4a1c3f470d3702.jpg_640x170_c1e8baeb.jpg'},
+              {id:'002',src:'/static/pics/banner/44fce1467be17702.jpg_640x170_ae6ad5e0.jpg'},
+              {id:'003',src:'/static/pics/banner/858481b3e2d65802.png_640x170_90ebc386.png'},
+              {id:'004',src:'/static/pics/banner/c72a1ccd4d7b2202.jpg_640x170_a4d1cf68.jpg'}
+            ]
           }
         }
     }
