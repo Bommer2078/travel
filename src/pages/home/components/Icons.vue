acfb35e3381180eb7c2160b32e33e8a2f55ae606
bommer <897694309@qq.com>
2018-07-27 15:38:38 +0800

    commit icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,76 @@
+<template>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="home-icons">
+          <div class="home-icon" v-for="item of page" :key="item.id">
+            <img :src="item.src"/>
+            <p>{{item.desc}}</p>
+          </div>
+        </div>
+      </swiper-slide>
+    </swiper>
+</template>
+
+<script>
+    export default {
+        name: "Icons",
+        data (){
+          return {
+            swiperOption:{
+              autoplay:false
+            },
+            icons:[
+              {id: "0001",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点旅游景点旅游景点旅游景点旅游景点'},
+              {id: "0002",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点'},
+              {id: "0003",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点'},
+              {id: "0004",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点'},
+              {id: "0005",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点'},
+              {id: "0006",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点'},
+              {id: "0007",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点'},
+              {id: "0008",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点'},
+              {id: "0008",src:'/static/pics/icons/ace00878a52d9702.png',desc:'旅游景点'}
+            ]
+          }
+        },
+        computed:{
+          pages (){
+            const pages = [];
+            this.icons.forEach((item,index)=>{
+              const page = Math.floor(index/8);
+              if(!pages[page]){
+                pages[page] = [];
+              }
+              pages[page].push(item);
+            })
+            return pages
+          }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+  @import '~style/ellipsis.less';
+  .home-icons{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    font-size: .12rem;
+    .home-icon{
+      width: 25%;
+      height: 25vw;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      > img{
+        width: 60%;
+        margin-bottom: .15rem;
+      }
+      > p {
+        width: 100%;
+        text-align: center;
+        .ellipsis
+      }
+    }
+  }
+</style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,16 +2,7 @@
   <div class="wrapper">
     <swiper :options="swiperOption">
       <swiper-slide>
-        <img src="@/assets/pics/8e4a1c3f470d3702.jpg_640x170_c1e8baeb.jpg">
-      </swiper-slide>
-      <swiper-slide>
-        <img src="@/assets/pics/44fce1467be17702.jpg_640x170_ae6ad5e0.jpg" height="170" width="640"/>
-      </swiper-slide>
-      <swiper-slide>
-        <img src="@/assets/pics/858481b3e2d65802.png_640x170_90ebc386.png" height="170" width="640"/>
-      </swiper-slide>
-      <swiper-slide>
-        <img src="@/assets/pics/c72a1ccd4d7b2202.jpg_640x170_a4d1cf68.jpg" height="170" width="640"/>
+        <img src="/static/pics/banner/8e4a1c3f470d3702.jpg_640x170_c1e8baeb.jpg">
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
@@ -42,6 +33,7 @@
     height:0;
     padding-bottom:31.25%;
     background: #eee;
+    margin-bottom: .05rem;
     img{
       width: 100%;
       height: 31.25vw;
